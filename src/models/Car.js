370ddab56310eea3bbc0614b76dcc171a09f6947
Vicent-Duarte <vicentduarte70@gmail.com>
2024-07-22 23:43:16 -0400

    creando car

---
+++ src/models/Car.js
@@ -0,0 +1,19 @@
+const Car = sequelize.define('car', {
+
+brand: {
+    type: DataTypes.STRING,
+    allowNull: false
+},
+    
+brand: {
+    type: DataTypes.STRING,
+    allowNull: false
+},
+
+brand: {
+    type: DataTypes.STRING,
+    allowNull: false
+},
+});
+
+module.exports = Car
